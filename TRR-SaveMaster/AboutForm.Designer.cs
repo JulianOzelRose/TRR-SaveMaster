a7f23ab4be4a235940148142cdb2524970817423
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2024-03-17 17:06:53 -0700

    Update version to 1.20

--- TRR-SaveMaster/AboutForm.Designer.cs
+++ TRR-SaveMaster/AboutForm.Designer.cs
@@ -52,9 +52,9 @@ namespace TRR_SaveMaster
             this.lblVersion.ForeColor = System.Drawing.Color.GreenYellow;
             this.lblVersion.Location = new System.Drawing.Point(408, 29);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(65, 36);
+            this.lblVersion.Size = new System.Drawing.Size(74, 36);
             this.lblVersion.TabIndex = 22;
-            this.lblVersion.Text = "v1.11";
+            this.lblVersion.Text = "v1.20";
             // 
             // lblAuthor
             // 
