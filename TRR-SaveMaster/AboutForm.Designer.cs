8951bc20229229e0c0f4d11054098d639366e41e
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2025-03-15 06:34:34 -0700

    Update version to 1.90

--- TRR-SaveMaster/AboutForm.Designer.cs
+++ TRR-SaveMaster/AboutForm.Designer.cs
@@ -52,7 +52,7 @@ namespace TRR_SaveMaster
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(116, 26);
             this.lblVersion.TabIndex = 22;
-            this.lblVersion.Text = "Version 1.85";
+            this.lblVersion.Text = "Version 1.90";
             // 
             // lblAuthor
             // 
