f997a6d3f4e4c4f8e6520feb9f128906d2997c09
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2025-07-05 17:59:30 -0700

    Remove platform compatibility warnings for TR 4-6

--- TRR-SaveMaster/MainForm.cs
+++ TRR-SaveMaster/MainForm.cs
@@ -467,38 +467,6 @@ namespace TRR_SaveMaster
             {
                 DisplayGameInfoTR3(cmbSavegamesTR3.SelectedItem as Savegame);
             }
-
-            ValidatePlatformSelection();
-        }
-
-        private void ValidatePlatformSelection()
-        {
-            if ((tabGame.SelectedIndex == TAB_TR4 || tabGame.SelectedIndex == TAB_TR5 || tabGame.SelectedIndex == TAB_TR6) && platform != Platform.PC)
-            {
-                string gameString = "";
-
-                if (tabGame.SelectedIndex == TAB_TR4)
-                {
-                    gameString = "Tomb Raider IV";
-                }
-                else if (tabGame.SelectedIndex == TAB_TR5)
-                {
-                    gameString = "Tomb Raider V";
-                }
-                else if (tabGame.SelectedIndex == TAB_TR6)
-                {
-                    gameString = "Tomb Raider VI";
-                }
-
-                string warningMessage = $"{platform.ToFriendlyString()} is not currently supported for {gameString}.";
-                MessageBox.Show(warningMessage, "Platform Not Supported", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
-                tsmiPC.Checked = true;
-                tsmiPlayStation4.Checked = false;
-                tsmiNintendoSwitch.Checked = false;
-                this.platform = Platform.PC;
-                this.Text = $"Tomb Raider I-VI Remastered Savegame Editor ({PlatformExtensions.ToFriendlyString(Platform.PC)})";
-            }
         }
 
         private void ReadConfigFile()
@@ -1965,8 +1933,6 @@ namespace TRR_SaveMaster
 
         private void tabGame_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ValidatePlatformSelection();
-
             if (tabGame.SelectedIndex == TAB_TR1)
             {
                 btnRefreshTR1.Enabled = !string.IsNullOrEmpty(savegamePathTRX) && File.Exists(savegamePathTRX);
